7b7e30f8a02701a5cb53fcde1005d1710b0f75d6
Evelyn Lockwood <evelyn.a.lockwood@gmail.com>
2018-02-10 17:59:17 -0500

    Adds Google Results Activity and Libraries

---
+++ app/src/main/java/com/example/susannahjones/bookassist/GoogleBookResultsActivity.java
@@ -0,0 +1,24 @@
+package com.example.susannahjones.bookassist;
+
+/**
+ * Created by Evelyn on 2/10/2018.
+ */
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class GoogleBookResultsActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googleresults);
+
+    }
+
+}
