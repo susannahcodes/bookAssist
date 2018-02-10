db131a7195a3c9afc30c164a3e43b51d2536c767
Susannah Jones <marysjones95@gmail.com>
2018-02-10 15:52:22 -0500

    added buttons

---
+++ app/src/main/java/com/example/susannahjones/bookassist/CameraActivity.java
@@ -0,0 +1,22 @@
+package com.example.susannahjones.bookassist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by susannahjones on 2/10/18.
+ */
+
+public class CameraActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+
+
+
+
+}
