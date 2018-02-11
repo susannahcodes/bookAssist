4e6b96f0f76a05138c55e23d579f1c26d3ea5932
Mengjia Luo <ml6uk@virginia.edu>
2018-02-10 22:10:12 -0500

    Updated UI

--- app/src/main/java/com/example/susannahjones/bookassist/MainActivity.java
+++ app/src/main/java/com/example/susannahjones/bookassist/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.my_main_image);
+        myImageView.setImageResource(R.drawable.library);
     }
 
 
